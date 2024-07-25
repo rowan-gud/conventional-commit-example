133db3ab17b754e66995df0a48bbf33c281969f8
Rowan Gudmundsson <rowan.gudmundsson@oddball.io>
2024-07-25 10:01:44 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+/// Adds two numbers
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::add;
+///
+/// let result = add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use conventional_commit_example::add;
+
+fn main() {
+    let a = 12;
+    let b = 14;
+
+    let sum = add(a, b);
+
+    println!("{a} + {b} = {sum}");
+}
