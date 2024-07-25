7df0ae6c042df9b5620d9513da70070977fd1747
Rowan Gudmundsson <rowan.gudmundsson@oddball.io>
2024-07-25 10:27:51 -0700

    feat: add subtract function

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,17 @@
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
+
+/// Subtract two numbers
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::subtract;
+///
+/// let result = subtract(3, 2);
+/// assert_eq!(result, 1);
+/// ```
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
