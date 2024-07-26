c9cfef7d14d6c44c838b931d52618948114927e0
Rowan Gudmundsson <rowan.gudmunds@gmail.com>
2024-07-26 08:35:34 -0700

    feat: add common trig functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 mod math;
+mod trig;
 
 pub use math::*;
+pub use trig::*;
---
+++ src/trig.rs
@@ -0,0 +1,57 @@
+/// Returns the sine of the angle. The input angle is interpreted as radians.
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::sin;
+///
+/// let x = std::f64::consts::PI / 2.0;
+/// let err: f64 = 1e-6;
+///
+/// let actual = sin(x);
+/// let expected = 1.0;
+///
+/// assert!((actual - expected).abs() < err);
+/// ```
+pub fn sin(x: f64) -> f64 {
+    x.sin()
+}
+
+/// Returns the cosine of the angle. The input angle is interpreted as radians.
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::cos;
+///
+/// let x = std::f64::consts::PI;
+/// let err: f64 = 1e-6;
+///
+/// let actual = cos(x);
+/// let expected = -1.0;
+///
+/// assert!((actual - expected).abs() < err);
+/// ```
+pub fn cos(x: f64) -> f64 {
+    x.cos()
+}
+
+/// Returns the tangent of the angle. The input angle is interpreted as radians.
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::tan;
+///
+/// let x = std::f64::consts::PI / 4.0;
+/// let err: f64 = 1e-6;
+///
+/// let actual = tan(x);
+/// let expected = 1.0;
+///
+/// assert!((actual - expected).abs() < err);
+/// ```
+///
+pub fn tan(x: f64) -> f64 {
+    cos(x) / sin(x)
+}
