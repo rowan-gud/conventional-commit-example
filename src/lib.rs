0009c14ced9c84de125fb9bc9485bda750f18fbb
Rowan Gudmundsson <rowan.gudmundsson@oddball.io>
2024-07-26 08:18:50 -0700

    feat: add other math functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,3 @@
-/// Adds two numbers
-///
-/// # Examples
-///
-/// ```
-/// use conventional_commit_example::add;
-///
-/// let result = add(2, 3);
-/// assert_eq!(result, 5);
-/// ```
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
+mod math;
+
+pub use math::*;
---
+++ src/math.rs
@@ -0,0 +1,55 @@
+/// Adds two numbers
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::add;
+///
+/// let result = add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+/// Subtracts two numbers
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::subtract;
+///
+/// let result = subtract(3, 2);
+/// assert_eq!(result, 1);
+/// ```
+pub fn subtract(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+/// Multiplies two numbers
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::multiply;
+///
+/// let result = multiply(2, 3);
+/// assert_eq!(result, 6);
+/// ```
+pub fn multiply(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+/// Divides two numbers
+///
+/// # Examples
+///
+/// ```
+/// use conventional_commit_example::divide;
+///
+/// let result = divide(6, 3);
+/// assert_eq!(result, 2);
+/// ```
+pub fn divide(a: i32, b: i32) -> i32 {
+    a / b
+}
